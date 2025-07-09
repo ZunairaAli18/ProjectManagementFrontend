477a640c4270263241ecc6c852cbc932aafb9584
ZunairaAli18 <zunaira.ali2053@gmial.com>
2025-07-09 12:28:03 +0500

    Routes setup

---
+++ src/app/signup/page.js
@@ -0,0 +1,7 @@
+export default function SignupPage() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl font-semibold">Signup Page</h1>
+    </div>
+  );
+}
