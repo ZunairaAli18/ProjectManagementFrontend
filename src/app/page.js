7de1017f11d3b66ab8cbdaf0b5235e9771043192
ZunairaAli18 <zunaira.ali2053@gmial.com>
2025-07-09 14:46:49 +0500

    Navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 'use client';
 import { useRouter } from "next/navigation";
+import Navbar from "./components/NavBar";
 
 export default function HomePage(){
   const router=useRouter();
@@ -7,11 +8,13 @@ export default function HomePage(){
   const handleSignUp=()=> router.push('/signup');
   return(
     <div>
+      <Navbar/>
       <h1 className="text-3xl font-bold">Welcome to HomePage</h1>
       <div className="space-x-4">
         <button onClick={handleLogin} className="bg-blue-600 text-white">Login</button>
         <button onClick={handleSignUp} className="bg-blue-600 text-white">SignUp</button>
       </div>
     </div>
+
   );
 }
